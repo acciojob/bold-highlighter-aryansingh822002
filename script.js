c7a52a010bdbc7f3c0cbf9fc9b898d645639531b
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2026-04-08 18:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
+    // 1. Select all <strong> elements on the page
+    const boldWords = document.querySelectorAll('strong');
 
-
+    // 2. Loop through each element and change the color to green
+    boldWords.forEach((word) => {
+        word.style.color = 'rgb(0, 128, 0)';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
+    // 1. Select all <strong> elements on the page
+    const boldWords = document.querySelectorAll('strong');
 
-    
+    // 2. Loop through each element and revert the color to black
+    boldWords.forEach((word) => {
+        word.style.color = 'rgb(0, 0, 0)';
+    });
 }
